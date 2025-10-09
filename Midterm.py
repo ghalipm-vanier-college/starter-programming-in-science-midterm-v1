452ec6f2770d35fe5eb88ea0a917a4643c54bc14
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-10-09 08:59:38 -0400

    Update Midterm.py
    main file update commit

--- Midterm.py
+++ Midterm.py
@@ -1,42 +1,29 @@
 import math
 
-# Q1: Calculate the area of an ellipse
-def area_of_ellipse(a, b):
-    return 0.0
+# Q1: Calculate the sum of even numbers
+def sum_of_even_nums_in(numbers):
+    # TODO: Implement this function
+    pass  # Replace with your code
 
-# Q2: Find the sum of numbers
-def sum_of_numbers(numbers):
-    return 0
+# Q2: Find the area of triangle:
+def area_of_triangle(a, b, c):
+    # TODO: Implement this function
+    pass  # Replace with your code
 
-# Q3: Upper left triangle
-def upper_left_triangle(n):
-    return ""
+# Q3:  Find the n-th row of Pascal’s triangle
+def pascal(n,k):
+    # TODO: Implement this function
+    pass  # Replace with your code
 
-# Q4: Hollow parallelogram with stars - left leaning
-def hollow_left_parallelogram(n):
-    return ""
+# Q4: Hollow triangle with upper left corner has right angle
+def hollow_triangle_with_upper_left_right_angle(n):
+    # TODO: Implement this function
+    pass  # Replace with your code
+
+# Q5: Rhombus shape
+def rhombus_shape(n):
+    # TODO: Implement this function
+    pass  # Replace with your code
 
 # ----------------------------------------------------------------
-print(sum_of_numbers([5, 2, 8, 10]))
-print()
 
-print(area_of_ellipse(5, 6))
-print()
-
-print(upper_left_triangle(2))
-print()
-
-print(upper_left_triangle(3))
-print()
-
-print(upper_left_triangle(5))
-print()
-
-print(hollow_left_parallelogram(2))
-print()
-
-print(hollow_left_parallelogram(3))
-print()
-
-print(hollow_left_parallelogram(4))
-print()
